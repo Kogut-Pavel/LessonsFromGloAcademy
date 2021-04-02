306d78470e94df663fe78dd6479a700394c733d8
Pavel Kogut <kogut.kogut1@mail.ru>
2021-04-02 18:21:18 +0500

    Обязательное задание

---
+++ script.js
@@ -0,0 +1,21 @@
+let money = 30000;
+let income = 'Фриланс';
+let addExpenses = 'Интернет, коммуналка, радости';
+let deposit = true;
+let mission = 100000;
+let period = 6;
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+
+console.log(addExpenses.length);
+
+console.log('Период равен ' + period + ' месяцев');
+console.log ('Цель заработать ' + mission + ' рублей');
+
+console.log(addExpenses.toLowerCase);
+console.log(addExpenses.split(' '));
+
+let budgetDay = money / 30;
+console.log(budgetDay);
