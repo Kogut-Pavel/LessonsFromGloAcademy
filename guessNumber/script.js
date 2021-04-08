3378fb6835510830f0ffb5d780cd293af7e1a929
Pavel Kogut <kogut.kogut1@mail.ru>
2021-04-08 23:29:22 +0500

    lesson06-hard done

--- guessNumber/script.js
+++ guessNumber/script.js
@@ -10,32 +10,44 @@ const getRandomInt = function(max) {
 
 function start() {
   let randomNumber = getRandomInt(100);
+  let numberAttempts = 10;
   function game() {
-      const num = prompt('Угадай число от 1 до 100');
-      if (num === null) {
-          alert('До свидания');
+    numberAttempts--;
+    if (numberAttempts < 1) {
+      if (confirm('Попытки закончились, хотите сыграть ещё?')) {
+        start();
+      } else {
+          alert('До свидания!');
           return;
       }
+    } else {
+        const num = prompt('Угадай число от 1 до 100');
+        if (num === null) {
+          alert('До свидания');
+          return;
+      } 
       if (isNumber(num)) {
-          if (num > randomNumber) {
-              alert('Загаданное число меньше');
-              game();
-          } else if (num < randomNumber) {
-              alert('Загаданное число больше');
-              game();
+        if (num > randomNumber) {
+          alert(`Загаданное число меньше, осталось попыток: ${numberAttempts}`);
+          game();
+        } else if (num < randomNumber) {
+            alert (`Загаданное число больше, осталось попыток: ${numberAttempts}`);
+            game();
+        } else {
+            if (confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть ещё?')) {
+            start();
           } else {
-              if (confirm('Вы угадали! Сыграем ещё?')) {
-                  start();
-              } else {
-                  alert('До свидания');
-                  return;
-              }
+              alert('До свидания');
+              return;
           }
+        } 
       } else {
           alert('Введите число');
           game();
       }
+    }
   }
+  console.dir(game);
   game();
 }
 
