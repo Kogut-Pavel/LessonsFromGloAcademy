999c48ddebbac045581a8c2095eb457ba58c8de2
Pavel Kogut <kogut.kogut1@mail.ru>
2021-04-28 20:13:01 +0500

    lesson20

--- script/script.js
+++ script/script.js
@@ -79,43 +79,44 @@ window.addEventListener('DOMContentLoaded', function() {
     const popup = document.querySelector('.popup'),
           popupBtn = document.querySelectorAll('.popup-btn'),
           popupContent = document.querySelector('.popup-content');
-          popup.addEventListener('click', (event) => {
-            let target = event.target;
-            if (target.classList.contains('popup-close')) {
-              popup.style.display = 'none';
-            } else {
-              target = target.closest('.popup-content');
-              if (!target) {
-                popup.style.display = 'none';
-              }
-            }
-          });
-          
-          
-  let count = 100;
-    const modalAnimate = () => {
-      let modalAnimateID = requestAnimationFrame(modalAnimate);   
-      if (count > 0) {
-        count -= 5;  
-        popupContent.style.transform = `translateY(${-count}%)`;
-      } else if (count < 0) {
-        cancelAnimationFrame(modalAnimateID);
-      }
-    };
-
-    popupBtn.forEach((elem) => {
-      elem.addEventListener("click", () => {
-        popup.style.display = "block";
-        const screenWidth = window.screen.width;
-        if (screenWidth > 768) {
-          modalAnimate();
+    popup.addEventListener('click', (event) => {
+    let target = event.target;
+      if (target.classList.contains('popup-close')) {
+        popup.style.display = 'none';
+      } else {
+        target = target.closest('.popup-content');
+        if (!target) {
+          popup.style.display = 'none';
         }
-      
-      });
+      }
     });
-  };
-
-  togglePopUp();
+          
+    let count = 100;
+      const modalAnimate = () => {
+        let modalAnimateID = requestAnimationFrame(modalAnimate);   
+        if (count > 0) {
+          count -= 5;            
+          popupContent.style.transform = `translateY(${-count}px)`;       
+        } 
+        if (count === 0) {       
+          cancelAnimationFrame(modalAnimateID);
+        }   
+      };
+          
+      popupBtn.forEach((elem) => {
+        elem.addEventListener("click", () => {
+          popup.style.display = "block";  
+          const screenWidth = window.screen.width;  
+            if (screenWidth > 768) { 
+            count = 100;    
+            modalAnimate(); 
+                 
+          }   
+        });    
+      });
+    };
+      
+   togglePopUp();
 
   // Tabs
 
